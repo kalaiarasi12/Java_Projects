177d38202ada8f471149e1dbed49d42a4ff52709
kalaiarasi12 <70111179+kalaiarasi12@users.noreply.github.com>
2025-06-08 22:39:20 +0530

    Add files via upload

---
+++ Marks_Cal.java
@@ -0,0 +1,17 @@
+package homeworkprogram;
+
+public class Marks_Cal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int math = 70;
+int eng = 50;
+int sci = 50;
+int tot = math+eng+sci;
+float avg = tot/3.0f ;
+System.out.println("Total Marks obtained "+ tot );
+System.out.println("Averge marks "+ avg );
+
+	}
+
+}
