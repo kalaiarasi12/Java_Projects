fd051a841f58e2e8b8cfc366bafbd812696ec11a
kalaiarasi12 <70111179+kalaiarasi12@users.noreply.github.com>
2025-06-08 22:20:40 +0530

    Add files via upload

--- AssignOper.java
+++ AssignOper.java
@@ -4,11 +4,15 @@ public class AssignOper {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int a =5;
-System.out.println("Assign and add "+(a+=2));
-System.out.println("Assign and Sub "+(a-=5));
-System.out.println("Assign and Multiply "+(a*=2));
-System.out.println(a);
+int a =7;
+// System.out.println("Assign and add "+(a+=2));
+// System.out.println("Assign and Sub "+(a-=5));
+// System.out.println("Assign and Multiply "+(a*=2));
+// System.out.println(a);
+int b = ++a;
+int c = a++;
+System.out.println("PreIncremental "+b);
+System.out.println("Post incremental "+a);
 	}
 
 }
