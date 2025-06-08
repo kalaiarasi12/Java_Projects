1766470b239a2b5100e1bb25ecaa6b49d1dce940
kalaiarasi12 <70111179+kalaiarasi12@users.noreply.github.com>
2025-06-08 21:40:29 +0530

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,19 @@
+package myfirstpackage;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int age = 25;
+float percentage = 87.34f;
+double average = 93.7865490;
+char grade ='A';
+boolean isThisTrue = true;
+System.out.println("Age = "+age);
+System.out.println("Percentage = "+percentage);
+System.out.println("Average = "+average);
+System.out.println("Grade = "+grade);
+System.out.println("IsThisTrue = "+isThisTrue);
+}
+
+}
