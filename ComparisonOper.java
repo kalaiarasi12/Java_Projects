ecc0a0f761fa5ce189ff2d642c103980cb3df576
kalaiarasi12 <70111179+kalaiarasi12@users.noreply.github.com>
2025-06-08 21:55:39 +0530

    Add files via upload

---
+++ ComparisonOper.java
@@ -0,0 +1,15 @@
+package myfirstpackage;
+
+public class ComparisonOper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 3;
+		int b = 4;
+		System.out.println("A Is Equal to b " +(a==b));
+		System.out.println("A Is Not Equal to b " +(a!=b));
+		System.out.println("A is Greater than b " +(a>=b));
+		System.out.println("A is smaller than b " +(a<=b));
+	}
+
+}
