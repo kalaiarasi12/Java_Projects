4b3faa4e0d1fbc1b1c0623752d347d99e4e74729
kalaiarasi12 <70111179+kalaiarasi12@users.noreply.github.com>
2025-06-14 05:45:53 +0530

    Add files via upload

---
+++ print_numbers.java
@@ -0,0 +1,20 @@
+package homeworkprogram;
+
+public class print_numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=1;i<=100;i++)
+		{
+			if (i%5 != 0)
+			{
+				System.out.println(i);
+				
+			}
+			continue;
+	
+		}
+
+	}
+
+}
