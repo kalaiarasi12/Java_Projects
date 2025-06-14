327a1d989fdefb2d0aaec829657f556798aa6e97
kalaiarasi12 <70111179+kalaiarasi12@users.noreply.github.com>
2025-06-14 05:44:59 +0530

    Add files via upload

---
+++ nestedLoop.java
@@ -0,0 +1,18 @@
+//Print all numbers between 1 and 100 that are divisible by both 3 and 5.
+package myfirstpackage;
+
+public class nestedLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for (int i =1; i<=100;i++)
+{
+	if(i%3 ==0 && i%5 ==0)
+	{
+	System.out.println(i);
+}
+	
+	}
+
+}
+}
