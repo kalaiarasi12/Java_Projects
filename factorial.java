c72b1265032588280abab28f29f9af558e588202
kalaiarasi12 <70111179+kalaiarasi12@users.noreply.github.com>
2025-08-09 14:35:13 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,17 @@
+package myfirstpackage;
+
+public class factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int fact =1;
+for (int i =1;i<=5;i++)
+{
+	fact = fact * i;
+	
+
+}
+System .out.println("Factorial "+ fact);
+	}
+
+}
